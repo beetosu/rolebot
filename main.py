13caf88fb18d112b48282078278d12cc99665a75
beetosu <cpwalsh@buffalo.edu>
2020-04-18 12:53:12 -0400

    lil fix

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def classlist(ctx):
     member = ctx.message.author
     for i in member.guild.roles:
         if str(i).isdigit():
-            classes.append(int(i))
+            classes.append(str(int(i)))
     classes.sort()
     catalog = ""
     for i in classes:
